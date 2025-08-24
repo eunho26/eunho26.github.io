7b8d2f84ebea944e65e797ad66c05537c913f097
eunho26 <57748699+eunho26@users.noreply.github.com>
2025-08-24 08:12:51 +0000

    Deploying to gh-pages from @ eunho26/eunho26.github.io@fae8b2663418e7169647036129c3c361e084b176 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,16 +398,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-competed-as-a-semi-finalist-in-the-ana-avatar-xprize-as-part-of-team-snu-miami-usa",
+          title: 'Competed as a semi-finalist in the ANA Avatar XPRIZE as part of Team...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
